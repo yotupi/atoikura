75e36334743b85d4ed56f44cbd27203e03376296
yotupi <321041858+yotupi@users.noreply.github.com>
2026-08-27 23:58:22 +0900

    Update to @ikura v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atoikura-v1";
+const CACHE = "atoikura-v3";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
